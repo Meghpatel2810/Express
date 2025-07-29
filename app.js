6d0c2692b50b16504e043a344d0b7c59c3dfa03b
MeghPatel2810 <meghpatel2810@gmail.com>
2025-07-29 19:56:41 +0530

    Started Mongodb operations

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+const Cat = mongoose.model('Cat', { no1 : Number , no2 : Number })
+
 const app = express()
 const port = 3000
 
@@ -30,11 +36,24 @@ app.get('/Sum',(req,res) => {
 })
 
 app.get('/SumProcess',(req,res) => {
+
+  
+
+
   var a=req.query.no1
   var b=req.query.no2
+
+
+  const kitty = new Cat({no1 : a,no2 : b})
+  kitty.save().then(() => console.log("Record Added"))
+
   var c=parseInt(a)+parseInt(b)
   res.send(`Number 1 : ${a} <br> Number 2 : ${b} <br> Sum : ${c}`)
 })
+
+
+
+
 
 app.get('/product/:id',(req,res) => {
   res.send("Value is "+req.params.id)
